1808aafa60a145ff14d9030c1adb35ecf0d6aabd
RafaelCSantana <rafa.vecindario.96@gmail.com>
2017-10-21 15:05:46 +0200

    Cuarta versión de Kata 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,11 +5,11 @@ import java.util.Map;
 public class Kata2 {
      public static void main(String [] arg) {
        
-         int [] data =  {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-         Histogram histo = new Histogram(data);
-         Map<Integer, Integer> histogram = histo.getHistogram();
+         String [] data = {"pepe", "juan", "carlos", "juan", "pepe"};
+         Histogram his = new Histogram(data);
+         Map<String, Integer> histogram = his.getHistogram();
         
-         for(int key: histogram.keySet()){
+         for(String key: histogram.keySet()){
             System.out.println(key + "==>" + histogram.get(key));
         }
     }     
